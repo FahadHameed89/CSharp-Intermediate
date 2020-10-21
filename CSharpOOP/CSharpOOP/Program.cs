6e60162bac6e70d6d0fde599e4937a39e21a1868
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-21 09:20:28 -0600

    Added gitignore and started talking about CLASSES

---
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+
+            Console.WriteLine("Please enter your first name:");
+            firstName = Console.ReadLine().Trim();
+
+            Console.WriteLine("Please enter your last name:");
+            lastName = Console.ReadLine().Trim();
+
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
+        }
+    }
+}
