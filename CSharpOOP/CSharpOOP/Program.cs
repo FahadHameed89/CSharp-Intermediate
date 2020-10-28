6f9adb8278c4c56f10b8a75335f67f12edcaffbd
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-28 10:16:27 -0600

    Talking About Object Lists;

--- CSharpOOP/CSharpOOP/Program.cs
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 
 namespace CSharpOOP
 {
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -11,8 +17,9 @@ namespace CSharpOOP
             // ----------
             // LINQ
             // ----------
-            // Create an empty list
-            List<string> myList = new List<string>() { "Hello", "World", "Here", "Are", "Some", "Strings", "Hello", "Hello", "World", "True", "False"};
+
+            // Initializer lists allow you to pre-populate lists when they are initialized.
+            List<string> myList = new List<string>() { "Hello", "World", "Here", "Are", "Some", "Strings", "Hello", "Hello", "World", "True", "False" };
 
             // Filtering (WHERE)
             myList.Where(x => x.Length > 4);
@@ -20,14 +27,12 @@ namespace CSharpOOP
 
             // Get Associated Values (SELECT)
             myList.Select(x => x.Length);
-            // 5, 5, 4, 3, 2, 6, 5, 5, 5, 4, 5
+            // 5, 5, 4, 3, 4, 6, 5, 5, 5, 4, 5
 
             // Combine the Two
             myList.Where(x => x.Length > 4).Select(x => x.Length);
-            // "Hello", "World", "Strings", "Hello", "Hello", "World", "False"
             // 5, 5, 6, 5, 5, 5, 5
 
-            // Aggregates
             List<int> myInts = new List<int>() { 6, 3, 10, 42, 5, 12, 37, 95, 1, 50, 100 };
 
             // Aggregates - Sum, Average, Max, Min
@@ -35,21 +40,18 @@ namespace CSharpOOP
             Console.WriteLine(myInts.Average());
             Console.WriteLine(myInts.Max());
             Console.WriteLine(myInts.Min());
+            // Keep in mind when stringing LINQ methods together, the Aggregate should be after any filtering and selection, as it will collapse the list into a single value.
 
-            // Keep in mind when stringing LINQ methods together, the Aggregate should be after any filtering and selection, as it will collapse the list into a single value. 
-
-            // Converting from a list of one item to a single item (Happens when you have a list of one item after filtering and selecting stuff)
-            myInts.Where(x => x == 12);
-            // Returns [12] (list format)
+            // Converting from a list of one item to a single item.
             myInts.Where(x => x == 12).Single();
-            // Returns 12 (int format)
-            // NOTE - Single will throw an exception if there is nothing in the list OR if there are multiple items in the list.
-            // If you don't want that exception thrown, you can use SingleOrDefault(), which will return the default for that type if it meets either of the previous failure states.
+            // 12
+
+            // This will throw an exception if there is nothing in the list, OR if there are multiple items in the list.
+            // If you don't want that exception thrown, you can use SingleOrDefault, which will return the default for that type if it meets either of the previous failure states.
             myInts.Where(x => x == 11).SingleOrDefault();
             // 0
 
-            // IF you don't want the multiple items failure state, you can use First, FirstOrDefault, Last, or LastOrDefault, which will still fail if there are not items, but will not fail if there are multiple items. 
-
+            // If you don't want the multiple items failure state, you can use First, FirstOrDefault, Last or LastOrDefault, which will still fail if there are no items, but will not fail if there are multiple.
             myInts.Where(x => x > 50).First();
             // 95
             myInts.Where(x => x == 12).First();
@@ -57,8 +59,43 @@ namespace CSharpOOP
             myInts.Where(x => x == 11).FirstOrDefault();
             // 0
 
-            // Order By is sort of like a Sort method except it works with DbSets (to be disucussed later)
-            myInts.OrderBy(x => x); 
+            // OrderBy is sort of like Sort, except it works with DbSets (to be discussed later).
+            myInts.OrderBy(x => x);
+
+            List<Person> myObjectList = new List<Person>()
+            {
+                new Person()
+                {
+                    FirstName = "Joe",
+                    LastName = "Smith",
+                    DateOfBirth = new DateTime(1990, 1, 1)
+                },
+                new Person()
+                {
+                    FirstName = "Sally",
+                    LastName = "Smith",
+                    DateOfBirth = new DateTime(1988, 2, 2)
+                },
+                new Person()
+                {
+                    FirstName = "Bob",
+                    LastName = "Jones",
+                    DateOfBirth = new DateTime(1992, 3, 3)
+                }
+            };
+
+            myObjectList.Where(x => x.LastName[0] == 'S');
+            // First character of last name is S.
+            // Joe, Sally
+
+            myObjectList.Select(x => x.DateOfBirth.Year);
+            // 1990, 1988, 1992
+
+            myObjectList.OrderBy(x => x.FirstName);
+            // Bob, Joe, Sally
+
+            myObjectList.Select(x => x.DateOfBirth.Year).Average();
+            // 1990
 
         }
     }
