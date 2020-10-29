daa44c5a14099380915a795b223d902410d041e8
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-29 11:29:20 -0600

    Added notes to Manufacturer.cs;

--- CSharpOOP/CSharpOOP/Models/Manufacturer.cs
+++ CSharpOOP/CSharpOOP/Models/Manufacturer.cs
@@ -5,24 +5,31 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CSharpOOP.Models
 {
+    // Data annotiations provide EF Core some information about the tables and columns in order to make an accurate model.
     // Defines the name of the table this model references.
     [Table("manufacturer")]
     public partial class Manufacturer
     {
-        // Constructor for initializing the virtual property
+        // Constructor for initializing the virtual property, required because we have the virtual collection. Initialized so it isn't null.
         public Manufacturer()
         {
             Vehicle = new HashSet<Vehicle>();
         }
-
+        // Specifies a PRIMARY KEY
         [Key]
+        // Specifies a columnName and data type. If the column name is not provided in the [Column], it will infer that it is identical to the property name
         [Column("ID", TypeName = "int(10)")]
         public int Id { get; set; }
+
+        // Specifies NOT NULL on nullable C# types (Like strings)
         [Required]
         [Column(TypeName = "varchar(25)")]
         public string Name { get; set; }
 
+        // InverseProperty draws a link to a property in another model (used for foreign keys)
         [InverseProperty("Manufacturer")]
+
+        // Prepresents the list of associated records through the foreign key (the records with this record as their parent)
         public virtual ICollection<Vehicle> Vehicle { get; set; }
     }
 }
