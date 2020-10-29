7144097f5f97e9b81e13f379bf9cf107f48813fb
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-29 10:50:04 -0600

    Updated Program.cs to add a new Vehicle to the DB;

--- CSharpOOP/CSharpOOP/Program.cs
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CSharpOOP.Models;
 
 namespace CSharpOOP
 {
@@ -10,8 +11,18 @@ namespace CSharpOOP
         static void Main(string[] args)
         {
             // Generate a Context
+            VehicleContext context = new VehicleContext();
 
+            context.Vehicle.Add(new Vehicle()
+            {
+                Id = 9,
+                Manufacturer = "Chevrolet",
+                Model = "Corvette",
+                ModelYear = 1959,
+                Colour = "Red"
+            });
 
+            context.SaveChanges();
         }
         
     }
