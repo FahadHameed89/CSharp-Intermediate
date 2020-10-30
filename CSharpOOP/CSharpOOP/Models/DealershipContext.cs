7832e1dabc2f66a81e89090dba5fa58ea37be39d
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-30 09:43:12 -0600

    Completed changes to Vehicle, next update will be to DealershipContext;

---
+++ CSharpOOP/CSharpOOP/Models/DealershipContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CSharpOOP.Models
+{
+    // Inheriting from Dbcontext provides us most of the functionality. The rest of the fileis customizing to suit our database. 
+    public partial class DealershipContext : DbContext
+    {
+    
+    }
+}
--- CSharpOOP/CSharpOOP/Models/Vehicle.cs
+++ CSharpOOP/CSharpOOP/Models/Vehicle.cs
@@ -2,31 +2,28 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
 
 namespace CSharpOOP.Models
-{ 
+{
     [Table("vehicle")]
-    public partial class Vehicle
+    class Vehicle
     {
         [Key]
         [Column("ID", TypeName = "int(10)")]
-        public int Id { get; set; }
-        [Column("ManufacturerID", TypeName = "int(10)")]
-        public int ManufacturerId { get; set; }
-        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string Manufacturer { get; set; }
+
         [Column(TypeName = "varchar(30)")]
         public string Model { get; set; }
-        [Column(TypeName = "int(10)")]
-        public int ModelYear { get; set; }
-        [Required]
-        [Column(TypeName = "varchar(25)")]
-        public string Colour { get; set; }
 
-        // Specifies the foreign key column itself (the one that has the value connected to the other table).
-        [ForeignKey(nameof(ManufacturerId))]
-        // Links to the virtual property in the parent.
-        [InverseProperty("Vehicles")]
-        // Represents the (singular) record for the parent.
-        public virtual Manufacturer Manufacturer { get; set; }
+        [Column(TypeName = "int(10)")]
+        public string ModelYear { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string Colour { get; set; }
     }
 }
