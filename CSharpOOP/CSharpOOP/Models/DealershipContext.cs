f0e9d9e11f242ad5526013441963796b658ebd66
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-30 09:31:52 -0600

    Setting up example for setting up a DB using C# first instead of having a DB first.;

--- CSharpOOP/CSharpOOP/Models/DealershipContext.cs
+++ CSharpOOP/CSharpOOP/Models/DealershipContext.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CSharpOOP.Models
 {
-    [Table("vehicle")]
-    class Vehicle
+    class DealershipContext
     {
-
     }
 }
