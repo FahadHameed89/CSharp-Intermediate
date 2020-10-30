0ddfec73a60d48340fec842f6bc9308ceed6041e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-30 10:10:18 -0600

    Added notes and completed step 5;

--- CSharpOOP/CSharpOOP/Program.cs
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -9,17 +9,21 @@ namespace CSharpOOP.Models
     {
         static void Main(string[] args)
         {
-           /*
-            *   Step 1 - Create Models folder
-            *   Step 2 - Create Context Class, and any required model classes
-            *   Step 3 - For each Class:
-            *   a - Name the tables for the model classes.
-            *   b - Declare Primary Key Property.
-            *   c - Specify other columns.
-            *   Step 4 - Setup Context.
-            *   - Inherit from DbContext
-            * **/
-           
+            /*
+             *   Step 1 - Create Models folder
+             *   Step 2 - Create Context Class, and any required model classes
+             *   Step 3 - For each Class:
+             *   a - Name the tables for the model classes.
+             *   b - Declare Primary Key Property.
+             *   c - Specify other columns.
+             *   Step 4 - Setup Context.
+             *   - Inherit from DbContext
+             *   - Declare table DbSets
+             *   - Implement OnConfiguring wiht Connection String
+             *   Implemenet OnModelCreating with string collation and foreign keys
+             *   Step 5 - Create a migration using 'dotnet ef migrations add initialMigration' in package manager console (in the project folder).
+             * **/
+
         }
     }
 }
